2bba72d2094cddac3e5d0ddd9b27e291f9fa4c4d
gwanhun <apple@APPLEui-MacBookPro.local>
2022-10-17 20:31:20 +0900

    초기세팅 수정사항 반영

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App/>}/>
+        <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
   );
 };
 
-export default App;
+export default Router;
