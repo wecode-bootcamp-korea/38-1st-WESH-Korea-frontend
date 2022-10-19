4b7f7d6ebeab632606db3b6427fd257ec86f7664
Kyu heum Choi <kyuheumchoi@Kyuui-MacBookPro.local>
2022-10-19 19:36:18 +0900

    git pull 후 conflict 수정

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignIn from './pages/Join/SignIn/SignIn';
+import SignUp from './pages/Join/SignUp/SignUp';
 import ProductList from './pages/ProductList/ProductList';
 import Nav from './components/Nav/Nav';
-import App from './App';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignIn />} />
         <Route path="/" element={<ProductList />} />
         <Route path="/nav" element={<Nav />} />
       </Routes>
