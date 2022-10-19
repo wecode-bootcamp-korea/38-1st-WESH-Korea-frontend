4d27ac27dedb646122dae9b2e27038e2ef9584dc
gwanhun <apple@APPLEui-MacBookPro.local>
2022-10-19 17:24:06 +0900

    [38기 정관훈]FIX: 머지

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
-import ProductDetail from './pages/ProductDetail/ProductDetail';
 import ProductList from './pages/ProductList/ProductList';
+import Nav from './components/Nav/Nav';
+import ProductDetail from './pages/ProductDetail/ProductDetail';
+import MainPage from './pages/MainPage/MainPage';
+import SignIn from './pages/Join/SignIn/SignIn';
+import SignUp from './pages/Join/SignUp/SignUp';
+import Cart from './pages/Cart/Cart';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ProductList />} />
+        <Route path="/" element={<Nav />} />
+        <Route path="/productDetail" element={<ProductDetail />} />
+        <Route path="/productlist" element={<ProductList />} />
+        <Route path="/mainpage" element={<MainPage />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
   );
