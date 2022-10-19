227c1e9733227d39b46c8ba8a42e1e4625b965a9
Kyu heum Choi <kyuheumchoi@Kyuui-MacBookPro.local>
2022-10-19 14:30:05 +0900

    MODIFY : signin삭제, router 수정

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import SignIn from './pages/Join/SignIn/SignIn';
+
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </BrowserRouter>
   );
