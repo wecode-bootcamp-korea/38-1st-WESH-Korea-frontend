49264698bc2b123963a25275d51ca8c2584b89fd
gwanhun <apple@APPLEui-MacBookPro.local>
2022-10-21 01:37:34 +0900

    [38기 정관훈]FIX:라우터 수정 및 애니메이션 구현

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
-// import ProductList from './pages/ProductList/ProductList';
+import ProductList from './pages/ProductList/ProductList';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
-// import SignUp from './Join/SignUp/SignUp';
-// import SignIn from './Join/SignIn/SignIn';
-// import Cart from './pages/Cart/Cart';
+import SignUp from './Join/SignUp/SignUp';
+import SignIn from './Join/SignIn/SignIn';
+import Cart from './pages/Cart/Cart';
 import MainPage from './pages/MainPage/MainPage';
 
 const Router = () => {
@@ -12,12 +12,12 @@ const Router = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Nav />} />
-        {/* <Route path="/productlist" element={<ProductList />} /> */}
+        <Route path="/productlist" element={<ProductList />} />
         <Route path="/productDetail" element={<ProductDetail />} />
         <Route path="/mainPage" element={<MainPage />} />
-        {/* <Route path="/signUp" element={<SignUp />} />
+        <Route path="/signUp" element={<SignUp />} />
         <Route path="/signIn" element={<SignIn />} />
-        <Route path="/cart" element={<Cart />} /> */}
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
   );
