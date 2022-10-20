6e0999086477eb173ef5585f310944a66971bd3e
woojong Choi <choiwoojong232@gmail.com>
2022-10-20 14:04:39 +0900

    ADD SNS로고 map 메서드로 수정

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
+
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Footer />} />
       </Routes>
     </BrowserRouter>
   );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "./styles/reset.scss"
-import "./styles/common.scss"
+import './styles/reset.scss';
+import './styles/common.scss';
 import Router from '../src/Router';
+import Footer from './components/Footer/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <Router />
-);
+root.render(<Router />);
