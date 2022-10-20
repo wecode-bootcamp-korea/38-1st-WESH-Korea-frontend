94c29a762235ba6514a5a3602a905891137e210a
gwanhun <apple@APPLEui-MacBookPro.local>
2022-10-20 15:55:47 +0900

    [38기 정관훈]ADD: 가격 변동 및 서버에 보낼 가격,카운트 state 저장

--- src/pages/ProductDetail/ProductTab.js
+++ src/pages/ProductDetail/ProductTab.js
@@ -4,14 +4,16 @@ import ProductReview from './ProductReview';
 import './ProductTab.scss';
 
 const ProductTab = () => {
-  const [data, setData] = useState([]);
+  const [detail, setDetail] = useState([]);
+  const [count, setCount] = useState(1);
+  const [price, setPrice] = useState(0);
   const [currentTab, setCurrentTab] = useState('제품정보');
 
   useEffect(() => {
     fetch(`/data/productdetail/detaildata.json
       `)
       .then(res => res.json())
-      .then(res => setData(res[0]));
+      .then(res => setDetail(res));
   }, []);
 
   const mappingObje = {
@@ -19,32 +21,66 @@ const ProductTab = () => {
     review: <ProductReview />,
   };
 
+  const up = () => {
+    setCount(count + 1);
+  };
+  const down = () => {
+    count !== 1 && setCount(count - 1);
+  };
+
+  const buyClick = () => {
+    setPrice(detail[0].price * count);
+  };
+
   return (
     <div className="product-tab">
-      {data && (
-        <div className="Product-tabBox">
-          <div className="top">
-            <img src={data.img} className="top-img" alt="data-main-image" />
-            <div className="right">
-              <div className="banner-detail">{data.detail}</div>
-              <div className="banner-title">{data.title}</div>
-              <div className="banner-price"> {data.price}</div>
-              <div className="banner-payment">
-                <button className="heart">♡</button>
-                <button className="bag">☗</button>
-                <button className="buy">바로구매</button>
+      {detail.map(detailInfo => {
+        return (
+          <div key={detailInfo.id} className="Product-tabBox">
+            <div className="top">
+              <img
+                src={detailInfo.img}
+                className="top-img"
+                alt="data-main-image"
+              />
+              <div className="right">
+                <div className="banner-detail">{detailInfo.detail}</div>
+                <div className="banner-title">{detailInfo.title}</div>
+                <div className="banner-price-box">
+                  <div className="banner-price-box-width">
+                    <div className="banner-price">
+                      {detailInfo.price * count}
+                    </div>
+                    <div className="button-box">
+                      <button className="plus" onClick={up}>
+                        +
+                      </button>
+                      <div className="count">{count}</div>
+                      <button className="mius" onClick={down}>
+                        -
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className="banner-payment">
+                  <button className="heart">♡</button>
+                  <button className="bag">☗</button>
+                  <button className="buy" onClick={buyClick}>
+                    바로구매
+                  </button>
+                </div>
               </div>
             </div>
+            <div className="menuTab">
+              <ul className="tabs">
+                <li onClick={() => setCurrentTab('info')}>제품정보</li>
+                <li onClick={() => setCurrentTab('review')}>제품리뷰</li>
+              </ul>
+              <div className="contents">{mappingObje[currentTab]}</div>
+            </div>
           </div>
-          <div className="menuTab">
-            <ul className="tabs">
-              <li onClick={() => setCurrentTab('info')}>제품정보</li>
-              <li onClick={() => setCurrentTab('review')}>제품리뷰</li>
-            </ul>
-            <div className="contents">{mappingObje[currentTab]}</div>
-          </div>
-        </div>
-      )}
+        );
+      })}
     </div>
   );
 };
