e03408b433a2d0e31d93602e80da18d8a10444ab
gwanhun <apple@APPLEui-MacBookPro.local>
2022-10-22 13:22:10 +0900

    [38기정관훈]FIX: 라우터 수정

--- src/Router.js
+++ src/Router.js
@@ -2,20 +2,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import SignIn from './pages/Join/SignIn/SignIn';
 import ProductList from './pages/ProductList/ProductList';
-import Nav from './components/Nav/Nav';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
 import MainPage from './pages/MainPage/MainPage';
-import Nav from './components/Nav/Nav';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/signin" element={<SignIn />} />
-        <Route path="/" element={<Nav />} />
-        <Route path="/productlist" element={<ProductList />} />
+        <Route path="/productlist/:categories" element={<ProductList />} />
         <Route path="/productDetail" element={<ProductDetail />} />
-        <Route path="/mainPage" element={<MainPage />} />
+        <Route path="/" element={<MainPage />} />
       </Routes>
     </BrowserRouter>
   );
