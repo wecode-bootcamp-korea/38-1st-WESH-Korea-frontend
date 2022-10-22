b5e92329712cedb241a3b9c17e295c58c8fa5aef
gwanhun <apple@APPLEui-MacBookPro.local>
2022-10-22 23:16:18 +0900

    [38기 정관훈]FIX: 레이아웃 조정

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -134,13 +134,15 @@ const Mainpage = () => {
           </div>
         </div>
       </div>
-      <div className="bottom-logo">WESH</div>
-      <div className="main-bottom">
-        <img
-          src="https://images.unsplash.com/photo-1541256996761-85df2efaa164?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-          alt="bath bomb"
-          className="main-bottom-img"
-        />
+      <div className="bottom-logo-box">
+        <div className="bottom-logo">WESH</div>
+        <div className="main-bottom">
+          <img
+            src="https://images.unsplash.com/photo-1541256996761-85df2efaa164?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+            alt="bath bomb"
+            className="main-bottom-img"
+          />
+        </div>
       </div>
     </>
   );
