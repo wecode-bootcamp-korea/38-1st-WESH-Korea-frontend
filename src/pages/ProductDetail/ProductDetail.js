1947ad63f2a60bd335baaa438cfa7b6a615e6768
gwanhun <apple@APPLEui-MacBookPro.local>
2022-10-22 14:58:14 +0900

    wip

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import Nav from '../../components/Nav/Nav';
 import ProductTab from './ProductTab';
+import Recommend from './Recommend';
 
 const ProductDetail = () => {
   return (
     <div>
       <Nav />
       <ProductTab />
+      <div className="recommend">
+        <Recommend />
+      </div>
     </div>
   );
 };
---
+++ src/pages/ProductDetail/Recommend.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+const Recommend = () => {
+  const [recommend, setRecommend] = useState([]);
+  useEffect(() => {
+    fetch();
+  }, []);
+  return (
+    <>
+      <div>dddd</div>
+      <div>dddd</div>
+      <div>dddd</div>
+    </>
+  );
+};
+export default Recommend;
