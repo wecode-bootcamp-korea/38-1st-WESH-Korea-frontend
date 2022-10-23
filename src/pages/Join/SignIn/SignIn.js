0d9bc433e38ca02a0dad124c7038901e040a9b05
Kyu heum Choi <kyuheumchoi@Kyuui-MacBookPro.local>
2022-10-23 14:10:36 +0900

    ADD : a태그로 외부페이지, navigate로 내부페이지 이동 기능 추가

--- src/pages/Join/SignIn/SignIn.js
+++ src/pages/Join/SignIn/SignIn.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './SignIn.scss';
 
 const SignIn = () => {
+  const navigate = useNavigate();
+
   const [loginInfo, setLoginInfo] = useState({
     email: '',
     password: '',
@@ -29,8 +31,7 @@ const SignIn = () => {
       .then(data => {
         if (data.data) {
           localStorage.setItem('token', data.data);
-          alert('로그인 성공');
-          Navigate('/mainPage', { replace: true });
+          navigate('/mainPage', { replace: true });
         } else {
           alert('아이디 혹은 비밀번호를 확인 해 주세요');
         }
@@ -65,9 +66,15 @@ const SignIn = () => {
           <button className="login-button">로그인</button>
         </form>
         <div className="etc">
-          <div className="etc-tag">회원가입</div>
-          <div className="etc-tag">위코드</div>
-          <div className="etc-tag">저스트코드</div>
+          <a className="etc-tag" onClick={() => navigate('/productlist')}>
+            회원가입
+          </a>
+          <a className="etc-tag" href="https://www.wecode.co.kr">
+            위코드
+          </a>
+          <a className="etc-tag" href="https://www.just-code.co.kr">
+            저스트코드
+          </a>
         </div>
         <div className="cannot">
           <div className="never">네이버 아이디로 로그인(준비중)</div>
