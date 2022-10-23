a2e09729f97ac57c898d264f3da769e78c27e506
gwanhun <apple@APPLEui-MacBookPro.local>
2022-10-23 16:39:35 +0900

    wip

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -28,10 +28,7 @@ const Mainpage = () => {
   useEffect(() => {
     resetTimeout();
     timeoutRef.current = setTimeout(
-      () =>
-        setCurrentIndex(prevIndex =>
-          prevIndex === eventData['eventImgs'].length - 1 ? 0 : prevIndex + 1
-        ),
+      () => setCurrentIndex(prevIndex => (prevIndex === 4 ? 0 : prevIndex + 1)),
       DELAY_TIME
     );
     return () => {
@@ -92,106 +89,106 @@ const Mainpage = () => {
   };
   const delay = 100;
   const onThrottleDragMove = throttle(onDragMove, delay);
-  console.log(eventData.eventImgs);
-  // console.log(eventData.data);
-  // console.log(eventData.data[0].eventImgs);
   return (
     <>
       <Nav />
-      <div className="mainpage-top">
-        <div className="main-center">
-          <div className="main-center-top">
-            <div className="thumbnail-box">
-              <div className="thumbnail-slide">
-                <div
-                  className="thumbnail"
-                  style={{
-                    transform: `translate3d(${-currentIndex * 100}%, 0, 0)`,
-                  }}
-                >
-                  {eventData.eventImgs.map(data => (
-                    <Thumbnail key={data.id} img={data.img} />
+      {eventData.eventImgs && eventData.bestProducts && (
+        <div className="mainpage-top">
+          <div className="main-center">
+            <div className="main-center-top">
+              <div className="thumbnail-box">
+                <div className="thumbnail-slide">
+                  <div
+                    className="thumbnail"
+                    style={{
+                      transform: `translate3d(${-currentIndex * 100}%, 0, 0)`,
+                    }}
+                  >
+                    {eventData.eventImgs.map(data => (
+                      <Thumbnail key={data.id} img={data.img} />
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="slideBox">
+                <div className="slideshowDots">
+                  {eventData.eventImgs.map((_, idx) => (
+                    <div
+                      key={idx}
+                      className={`slideshowDot${
+                        currentIndex === idx ? ' active' : ''
+                      }`}
+                      onClick={() => {
+                        setCurrentIndex(idx);
+                      }}
+                    ></div>
                   ))}
                 </div>
               </div>
             </div>
-            <div className="slideBox">
-              <div className="slideshowDots">
-                {eventData.eventImgs.map((_, idx) => (
+            <div className="main-middle">
+              <div className="best-tag-box">
+                <ul className="best-tag">
+                  <div className="tag-margin">#슬리피기프트</div>
+                  <div className="tag-margin">#네이키드</div>
+                  <div className="tag-margin">#신선한 재료</div>
+                </ul>
+                <div className="best-item-image-box">
+                  <img
+                    alt="best-item-tag"
+                    className="best-item-image"
+                    src="https://images.unsplash.com/photo-1637675416143-7b11da3fc2c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                  />
+                </div>
+                <div className="best-item-list-box">
+                  <div className="best-best">
+                    <Link to="productlist/all?limit=16&offset=0">BEST →</Link>
+                  </div>
                   <div
-                    key={idx}
-                    className={`slideshowDot${
-                      currentIndex === idx ? ' active' : ''
-                    }`}
-                    onClick={() => {
-                      setCurrentIndex(idx);
-                    }}
-                  ></div>
-                ))}
+                    className="best-item-list"
+                    onMouseDown={onDragStart}
+                    onMouseMove={isDrag ? onThrottleDragMove : null}
+                    onMouseUp={onDragEnd}
+                    onMouseLeave={onDragEnd}
+                    ref={scrollRef}
+                  >
+                    {eventData.bestProducts.map(best => (
+                      <Best
+                        key={best.id}
+                        img={best.img}
+                        title={best.title}
+                        price={best.price}
+                      />
+                    ))}
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="main-middle">
-            <div className="best-tag-box">
-              <ul className="best-tag">
-                <div className="tag-margin">#슬리피기프트</div>
-                <div className="tag-margin">#네이키드</div>
-                <div className="tag-margin">#신선한 재료</div>
-              </ul>
-              <div className="best-item-image-box">
+            <div className="main-bottom">
+              <div className="introduce">
                 <img
-                  alt="best-item-tag"
-                  className="best-item-image"
-                  src="https://images.unsplash.com/photo-1637675416143-7b11da3fc2c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                  className="introduce-image"
+                  src="https://images.unsplash.com/photo-1521220609214-a8552380c7a4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1774&q=80"
+                  alt="playing-img"
                 />
               </div>
-              <div className="best-item-list-box">
-                <div className="best-best">
-                  <Link to="productlist/all?limit=16&offset=0">BEST →</Link>
+              <div className="message-box">
+                <div className="message">
+                  "<div className="point">위쉬</div>는 자연에서 얻은 신선한
+                  재료와
                 </div>
-                <div
-                  className="best-item-list"
-                  onMouseDown={onDragStart}
-                  onMouseMove={isDrag ? onThrottleDragMove : null}
-                  onMouseUp={onDragEnd}
-                  onMouseLeave={onDragEnd}
-                  ref={scrollRef}
-                >
-                  {eventData.bestProducts.map(best => (
-                    <Best
-                      key={best.id}
-                      img={best.img}
-                      title={best.title}
-                      price={best.price}
-                    />
-                  ))}
+                <div className="message">
+                  동물실험을 하지 않은 <div className="point">정직한</div>{' '}
+                  재료를 사용하여
                 </div>
-              </div>
-            </div>
-          </div>
-          <div className="main-bottom">
-            <div className="introduce">
-              <img
-                className="introduce-image"
-                src="https://images.unsplash.com/photo-1521220609214-a8552380c7a4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1774&q=80"
-                alt="playing-img"
-              />
-            </div>
-            <div className="message-box">
-              <div className="message">
-                "<div className="point">위쉬</div>는 자연에서 얻은 신선한 재료와
-              </div>
-              <div className="message">
-                동물실험을 하지 않은 <div className="point">정직한</div> 재료를
-                사용하여
-              </div>
-              <div className="message">
-                <div className="point">모든 제품</div>을 손으로 만듭니다."
+                <div className="message">
+                  <div className="point">모든 제품</div>을 손으로 만듭니다."
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
       <div className="bottom-logo-box">
         <div className="bottom-logo">WESH</div>
         <div className="main-bottom">
