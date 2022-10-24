06f26a384dce36cfa30968018c605c113705f855
gwanhun <apple@APPLEui-MacBookPro.local>
2022-10-24 15:58:02 +0900

    wip

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import ProductTab from './ProductTab';
-import Recommend from './Recommend';
 
 import Nav from '../../components/Nav/Nav';
 
@@ -10,7 +9,7 @@ const ProductDetail = () => {
   const { id } = useParams();
 
   useEffect(() => {
-    fetch(`http://10.58.52.112:8000/product/${id}
+    fetch(`http://10.58.52.123:8000/product/${id}
       `)
       .then(res => res.json())
       .then(res => setDetail(res.detailPageData));
@@ -22,9 +21,6 @@ const ProductDetail = () => {
       {detail && (
         <>
           <ProductTab detail={detail} />
-          <div className="recommend">
-            <Recommend detail={detail} />
-          </div>
         </>
       )}
     </div>
