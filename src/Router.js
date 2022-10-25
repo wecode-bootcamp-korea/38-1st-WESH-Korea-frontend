199a5c66c20bcb09dae93e625209db578c22c187
gwanhun <apple@APPLEui-MacBookPro.local>
2022-10-25 17:34:29 +0900

    [38기 정관훈]FIX: 리뷰 반영

--- src/Router.js
+++ src/Router.js
@@ -5,10 +5,13 @@ import MainPage from './pages/MainPage/MainPage';
 import Cart from './pages/Cart/Cart';
 import SignIn from './pages/Join/SignIn/SignIn';
 import SignUp from './pages/Join/SignUp/SignUp';
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/product-list/:categories" element={<ProductList />} />
@@ -17,6 +20,7 @@ const Router = () => {
         <Route path="/Sign-up" element={<SignUp />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
