1a4aaa9daaac497a783d9a854eec27b624109331
Kyu heum Choi <kyuheumchoi@Kyuui-MacBookPro.local>
2022-10-25 14:50:18 +0900

    불필요한 주석 삭제

---
+++ src/Router.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import SignUp from './pages/Join/SignUp/SignUp';
+import Nav from './components/Nav/Nav';
+import ProductList from './pages/ProductList/ProductList';
+import ProductDetail from './pages/ProductDetail/ProductDetail';
+import MainPage from './pages/MainPage/MainPage';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/productlist" element={<ProductList />} />
+        <Route path="/productDetail" element={<ProductDetail />} />
+        <Route path="/mainPage" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
