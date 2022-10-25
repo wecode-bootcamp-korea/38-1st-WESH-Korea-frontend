098652afc1c962ca4afa7e409e51aae473be6c74
gwanhun <apple@APPLEui-MacBookPro.local>
2022-10-25 17:47:44 +0900

    [38기 정관훈]FIX: 링크 수정

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const BASE_URL_LIST = 'http://10.58.52.125:3000';
 const BASE_URL_CART = 'http://10.58.52.56:8000';
 
 const API = {
-  list: `${BASE_URL_LIST}/productlist/`,
+  list: `${BASE_URL_LIST}/productlist`,
   detail: `${BASE_URL_DETAIL}/product`,
   main: `${BASE_URL_DETAIL}`,
   cart: `${BASE_URL_CART}/cart`,
