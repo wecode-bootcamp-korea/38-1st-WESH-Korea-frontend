c180e51a9b5b5c88f1bd07c3951d6c1be66480f3
gwanhun <apple@APPLEui-MacBookPro.local>
2022-10-25 17:08:40 +0900

    [38기 정관훈]FIX: 라우터 수정

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -7,24 +7,28 @@ import {
 } from 'react-router-dom';
 import Product from './Product/Product';
 import Banner from './Banner';
-
 import Nav from '../../components/Nav/Nav';
-import './Product-list.scss';
+import './ProductList.scss';
 
-const Productlist = () => {
-  const [productdata, setProductdata] = useState([]);
+const ProductList = () => {
+  const [data, setData] = useState([]);
   const [searchParams, setSearchPhams] = useSearchParams();
+  const [isLoading, setIsLoading] = useState(true);
   const limit = searchParams.get('limit');
   const offset = searchParams.get('offset');
   const [sort, setSort] = useState('high');
   const { categories } = useParams();
 
   useEffect(() => {
+    setIsLoading(true);
     fetch(
-      `http://10.58.52.125:3000/product-list/${categories}?sort=${sort}&offset=${offset}&limit=${limit}`
+      `http://10.58.52.125:3000/productlist/${categories}?sort=${sort}&offset=${offset}&limit=${limit}`
     )
       .then(res => res.json())
-      .then(res => setProductdata(res.data));
+      .then(res => {
+        setData(res.data);
+        setIsLoading(false);
+      });
   }, [offset, limit, categories, sort]);
 
   const goPage = pageNumber => {
@@ -37,123 +41,119 @@ const Productlist = () => {
     setSort(e.target.value);
   };
 
+  if (isLoading) return;
+
   return (
     <>
       <Nav />
-      {productdata && (
-        <div className="product-list">
-          <Banner />
-          <div className="list-page">
-            <div className="menu-bar">
-              <div className="filter">
-                <div className="all">
-                  <Link
-                    to={`/product-list/all?sort=best&offset=0&limit=16`}
-                    className="link"
-                  >
-                    전체상품
-                  </Link>
-                  <Link
-                    to={`/product-list/soap?sort=best&offset=0&limit=16`}
-                    className="link"
-                  >
-                    비누
-                  </Link>
-                  <Link
-                    to={`/product-list/lotion?sort=best&offset=0&limit=16`}
-                    className="link"
-                  >
-                    로션
-                  </Link>
-                  <Link
-                    to={`/product-list/oil?sort=best&offset=0&limit=16`}
-                    className="link"
-                  >
-                    오일
-                  </Link>
-                  <Link
-                    to={`/product-list/perfume?sort=best&offset=0&limit=16`}
-                    className="link"
-                  >
-                    향수
-                  </Link>
-                </div>
+      <div className="product-list">
+        <Banner />
+        <div className="list-page">
+          <div className="menu-bar">
+            <div className="filter">
+              <div className="all">
+                <Link
+                  to={`/product-list/all?sort=best&offset=0&limit=16`}
+                  className="link"
+                >
+                  전체상품
+                </Link>
+                <Link
+                  to={`/product-list/soap?sort=best&offset=0&limit=16`}
+                  className="link"
+                >
+                  비누
+                </Link>
+                <Link
+                  to={`/product-list/lotion?sort=best&offset=0&limit=16`}
+                  className="link"
+                >
+                  로션
+                </Link>
+                <Link
+                  to={`/product-list/oil?sort=best&offset=0&limit=16`}
+                  className="link"
+                >
+                  오일
+                </Link>
+                <Link
+                  to={`/product-list/perfume?sort=best&offset=0&limit=16`}
+                  className="link"
+                >
+                  향수
+                </Link>
               </div>
-              <select className="order" onChange={goSort}>
-                <option className="order-best">
-                  <Link
-                    to={`/product-list/${categories}?sort=best&offset=0&limit=16`}
-                  >
-                    best
-                  </Link>
-                </option>
-                <option className="order-low">
-                  <Link
-                    to={`/product-list/${categories}?sort=low&offset=0&limit=16`}
-                  >
-                    low
-                  </Link>
-                </option>
-                <option className="order-high">
-                  <Link
-                    to={`/product-list/${categories}?sort=high&offset=0&limit=16`}
-                  >
-                    high
-                  </Link>
-                </option>
-                <option className="order-review">
-                  <Link
-                    to={`/product-list/${categories}?sort=review&offset=0&limit=16`}
-                  >
-                    review
-                  </Link>
-                </option>
-              </select>
             </div>
-            <div className="page-box">
-              {product -
-                data
-                  .slice(0, 16)
-                  .map(list => (
-                    <Product
-                      key={list.id}
-                      id={list.id}
-                      title={list.title}
-                      img={list.img}
-                      price={list.price}
-                      tag={list.tag}
-                      category={list.category}
-                    />
-                  ))}
-            </div>
-            <div className="button-click">
-              <button onClick={() => goPage(1)} className="btn">
-                ◀️◀️
-              </button>
-              <button onClick={() => goPage(1)} className="btn">
-                1
-              </button>
-              <button onClick={() => goPage(2)} className="btn">
-                2
-              </button>
-              <button onClick={() => goPage(3)} className="btn">
-                3
-              </button>
-              <button onClick={() => goPage(4)} className="btn">
-                4
-              </button>
+            <select className="order" onChange={goSort}>
+              <option className="order-best">
+                <Link
+                  to={`/product-list/${categories}?sort=best&offset=0&limit=16`}
+                >
+                  best
+                </Link>
+              </option>
+              <option className="order-low">
+                <Link
+                  to={`/product-list/${categories}?sort=low&offset=0&limit=16`}
+                >
+                  low
+                </Link>
+              </option>
+              <option className="order-high">
+                <Link
+                  to={`/product-list/${categories}?sort=high&offset=0&limit=16`}
+                >
+                  high
+                </Link>
+              </option>
+              <option className="order-review">
+                <Link
+                  to={`/product-list/${categories}?sort=review&offset=0&limit=16`}
+                >
+                  review
+                </Link>
+              </option>
+            </select>
+          </div>
+          <div className="page-box">
+            {data.slice(0, 16).map(e => (
+              <Product
+                key={e.id}
+                id={e.id}
+                title={e.title}
+                img={e.img}
+                price={e.price}
+                tag={e.tag}
+                category={e.category}
+              />
+            ))}
+          </div>
+          <div className="button-click">
+            <button onClick={() => goPage(1)} className="btn">
+              ◀️◀️
+            </button>
+            <button onClick={() => goPage(1)} className="btn">
+              1
+            </button>
+            <button onClick={() => goPage(2)} className="btn">
+              2
+            </button>
+            <button onClick={() => goPage(3)} className="btn">
+              3
+            </button>
+            <button onClick={() => goPage(4)} className="btn">
+              4
+            </button>
 
-              <button onClick={() => goPage(4)} className="btn">
-                ▶▶
-              </button>
-            </div>
+            <button onClick={() => goPage(4)} className="btn">
+              ▶▶
+            </button>
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 };
 
-export default Productlist;
-
+export default ProductList;
 const PAGELIMIT = 16;
