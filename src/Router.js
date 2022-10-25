3a0991a1b6be92d62ad236462ab2b8fe5970cd4f
gwanhun <apple@APPLEui-MacBookPro.local>
2022-10-25 12:01:26 +0900

    [38기 정관훈]ADD: 라우터 최종

--- src/Router.js
+++ src/Router.js
@@ -1,21 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import SignUp from './pages/Join/SignUp/SignUp';
 import ProductList from './pages/ProductList/ProductList';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
 import MainPage from './pages/MainPage/MainPage';
-import Nav from './components/Nav/Nav';
+import Cart from './pages/Cart/Cart';
+import SignIn from './pages/Join/SignIn/SignIn';
+import SignUp from './pages/Join/SignUp/SignUp';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Nav />
       <Routes>
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/productlist" element={<ProductList />} />
-        <Route path="/productDetail" element={<ProductDetail />} />
         <Route path="/" element={<MainPage />} />
+        <Route path="/product-list/:categories" element={<ProductList />} />
+        <Route path="/product-detail/:id" element={<ProductDetail />} />
+        <Route path="/Sign-in" element={<SignIn />} />
+        <Route path="/Sign-up" element={<SignUp />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
   );
