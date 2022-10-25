114e9049c3fe4a8d563d9a1939c17bd7587fdbfb
gwanhun <apple@APPLEui-MacBookPro.local>
2022-10-25 20:00:14 +0900

    [38기 정관훈]FIX: footer 수정

--- src/Router.js
+++ src/Router.js
@@ -4,9 +4,9 @@ import SignUp from './pages/Join/SignUp/SignUp';
 import ProductList from './pages/ProductList/ProductList';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
 import MainPage from './pages/MainPage/MainPage';
-import Cart from './pages/Cart/Cart';
-import SignIn from './pages/Join/SignIn/SignIn';
-import SignUp from './pages/Join/SignUp/SignUp';
+// import Cart from './pages/Cart/Cart';
+// import SignIn from './pages/Join/SignIn/SignIn';
+// import SignUp from './pages/Join/SignUp/SignUp';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
 
@@ -18,9 +18,9 @@ const Router = () => {
         <Route path="/" element={<MainPage />} />
         <Route path="/product-list/:categories" element={<ProductList />} />
         <Route path="/product-detail/:id" element={<ProductDetail />} />
-        <Route path="/Sign-in" element={<SignIn />} />
+        {/* <Route path="/Sign-in" element={<SignIn />} />
         <Route path="/Sign-up" element={<SignUp />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route path="/cart" element={<Cart />} /> */}
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,76 +3,80 @@ import './Footer.scss';
 
 const Footer = () => {
   return (
-    <div className="footerWrap">
+    <div className="footer-wrap">
       <div className="top-footer">38th-1st-Wesh</div>
-      <div className="footerInner">
-        <div className="footerMainWrap">
-          <div className="footerLeftSide">
+      <div className="footer-inner">
+        <div className="footer-main-wrap">
+          <div className="footer-left-side">
             <input
-              className="inputTab"
+              className="input-tab"
               type="email"
               name="구독 인풋창"
               placeholder="이메일 주소를 입력해주세요"
             />
-            <p className="footerSubInfor">
+            <p className="footer-sub-infor">
               매주 월요일 오후, 구독자님들을 위한 제품과 브랜드 이야기를 전해
               드립니다. 구독은 언제든지 해지하실 수 있습니다. 미리보기
             </p>
-            <div className="footerCallInfo">
-              <section className="footerCallSub">
+            <div className="footer-call-info">
+              <section className="footer-call-sub">
                 <span>고객센터</span>
                 <span>1234-5678</span>
-                <span className="emailUnder">wecode@wesh.co.kr</span>
+                <span className="email-under">wecode@wesh.co.kr</span>
                 <span>상담전화 13:00~16:00(평일)</span>
                 <span>상담톡 10:00~16:00(평일)</span>
               </section>
-              <section className="footerCoporInfo">
+              <section className="footer-copor-info">
                 <span>기업 선물</span>
                 <span>070-7777-7777</span>
                 <span>wesh@kkk.co.kr</span>
               </section>
             </div>
-            <ul className="footerSnsLink">
+            <ul className="footer-sns-link">
               <li>
                 <img
-                  className="TwiterImg"
+                  className="twiter-img"
                   link="https://twitter.com/?lang=ko"
                   src="https://www.lush.co.kr/content/renewal/mobile/images/ico/ico_twitter.png"
+                  alt="twitter-img"
                 ></img>
               </li>
               {FOOTER_SNS_IMG.map(logo => {
-                return (
-                  <img src={logo.image} key={logo.id} alt={'logo-image'} />
-                );
+                return <img src={logo.image} key={logo.id} alt="logo-image" />;
               })}
             </ul>
           </div>
-          <div className="footerRightSide">
+          <div className="footer-right-side">
             <div>
-              <div className="footerNotice">
+              <div className="footer-notice">
                 <span>공지사항</span>
                 <span>2022-10-18 [공지]카카오 블랙 아웃 제발 그만~~</span>
                 <span>2022-10-19 [공지]프론트/백 화이팅~~~</span>
               </div>
               <div>
-                <div className="footerMenuList">
-                  <ul className="footMenuGroup1">
+                <div className="footer-menu-list">
+                  <ul className="foot-menu-group1">
                     <li>러쉬 소개</li>
-                    <li className="footerMenuPersonInfo">개인정보처리방침</li>
-                    <li className="footerMenuFAQ">FAQ</li>
+                    <li className="footer-menu-person-info">
+                      개인정보처리방침
+                    </li>
+                    <li className="footer-menu-FAQ">FAQ</li>
                   </ul>
-                  <ul className="footMenuGroup2">
+                  <ul className="foot-menu-group2">
                     <li>스카우트</li>
-                    <li className="footerMenuVideoInfo">영상정보관리지침</li>
+                    <li className="footer-menuV\-video-info">
+                      영상정보관리지침
+                    </li>
                     <li>1:1문의</li>
                   </ul>
-                  <ul className="footMenuGroup3">
+                  <ul className="foot-menu-group3">
                     <li>채용안내</li>
-                    <li className="footerUsePolicy">이용약관</li>
+                    <li className="footer-use-policy">이용약관</li>
+                    <li></li>
                   </ul>
                 </div>
               </div>
-              <div className="footerPlaceInfo">
+              <div className="footer-place-info">
                 <p>서울 강남구 테헤란로 위워크타워 10층 위코드 선릉 2호점</p>
                 <p>사이트 운영자: 주식회사 위시코리아 | 대표이사 : 공석</p>
                 <p>사업자 등록번호: 201-81-88888 사업자정보확인</p>
@@ -80,7 +84,7 @@ const Footer = () => {
                   통신판매업 신고번호: 2022-서울강남-984723 | 개인정보보호책임자
                   : 공석
                 </p>
-                <p className="foorterCopyRight">
+                <p className="foorter-copy-right">
                   COPYRIGHT@WESHKOREA.CO.LTD.ALL RIGHTS RESERVED
                 </p>
               </div>
