efb95410bc69d17ef9943f77cd67cd5e201a8402
YunKukPark <a01043340999@gmail.com>
2022-10-25 14:51:48 +0900

    LiveCodeReview -1

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -6,19 +6,26 @@ import Recommend from './Recommend';
 
 const ProductDetail = () => {
   const [detail, setDetail] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const { id } = useParams();
 
   useEffect(() => {
+    setIsLoading(true);
     fetch(`http://10.58.52.87:8000/product/${id}
       `)
       .then(res => res.json())
-      .then(res => setDetail(res.detailPageData));
-  }, []);
+      .then(res => {
+        setDetail(res.detailPageData);
+        setIsLoading(false);
+      });
+  }, [id]);
+
+  if (isLoading) return; // 스켈리톤 UI loading bar
 
   return (
     <div>
       <Nav />
-      {detail && <ProductTab detail={detail} />}
+      <ProductTab detail={detail} />
       <div className="recommend">
         <Recommend />
       </div>
